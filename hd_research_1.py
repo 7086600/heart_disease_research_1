7577d145b58b76e22def80bb0ff49ef3504635d2
Alexander <7086600@gmail.com>
2025-05-26 05:59:38 +0300

    implementing one-sample t-tests for two groups of patients: with heart disease and without

--- hd_research_1.py
+++ hd_research_1.py
@@ -1,6 +1,7 @@
 # import necessary libraries
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 # load and inspect the dataset
 hd = pd.read_csv('heart_disease.csv', sep=',')
@@ -21,11 +22,35 @@ yes_hd = hd.loc[yes_filter]
 no_hd = hd.loc[no_filter]
 
 # task 1: save cholesterol levels for patients with heart disease
-chol_hd = yes_hd['chol']
+yes_chol_hd = yes_hd['chol']
 print()
 
 # task 2: Calculate the mean cholesterol level for patients who were diagnosed with heart disease
-mean_chol_hd = np.mean(chol_hd)
+mean_chol_hd = np.mean(yes_chol_hd)
 print(f'Mean cholesterol level for patients with heart disease: {mean_chol_hd:.2f}')
 print("This level is higher than 240 mg/dl and and therefore unhealthy.")
 print()
+
+# task 3: Implementing a One-Sample T-Test
+# hypotheses:
+# H0: The mean cholesterol level for patients with heart disease is equal to 240 mg/dl
+# H1: The mean cholesterol level for patients with heart disease is greater than 240 mg/dl
+
+_, p_value = ttest_1samp(yes_chol_hd, 240.00)
+print(f'Two sides p-value for the one-sample t-test for patients with heart disease: {p_value:.4f}')
+print(f"One-sided p-value for the one-sample t-test for patients with heart disease: {p_value / 2:.4f}")
+
+# task 4: Interpret the results
+print("Using a significance threshold of 0.05 we can conclude that the mean cholesterol level for patients with heart disease is significantly higher than 240 mg/dl.")
+
+# task 5: step 1-4 for patients without heart disease
+no_chol_hd = no_hd['chol']
+print()
+mean_chol_hd = np.mean(no_chol_hd)
+print(f'Mean cholesterol level for patients without heart disease: {mean_chol_hd:.2f}')
+print("This level is greater than 240 mg/dl and therefore also unhealthy.")
+print()
+_, p_value = ttest_1samp(no_chol_hd, 240.00)
+print(f'Two sides p-value for the one-sample t-test for patients without heart disease: {p_value:.4f}')
+print(f"One-sided p-value for the one-sample t-test for patients without heart disease: {p_value / 2:.4f}")
+print("Using a significance threshold of 0.05 we can conclude that the mean cholesterol level for patients without heart disease is significantly lower than 240 mg/dl.")
